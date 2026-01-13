ff0b1e743d83125cc0e8c5707526985a2b4daa88
miloa2002 <agudelo.tapias@gmail.com>
2026-01-13 17:51:55 -0500

    FEAT: Add to cart, increase quantity

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,8 @@
 import { numberPrice } from "../utils";
 
-export default function Product({ product }) {
+export default function Product({ product, cart = [], addToCart, increaseQuantity }) {
+  const productExistsInCart = cart.find((element) => element.id === product.id);
+
   return (
     <div>
       <div className="relative">
@@ -16,27 +18,35 @@ export default function Product({ product }) {
           />
         </picture>
 
-        <button className="flex items-center justify-center absolute bottom-0 left-0 right-0 translate-y-1/2 bg-white border border-rose-950 rounded-full font-semibold text-rose-950 w-2/3 m-auto p-2 cursor-pointer">
-          <img src="/images/icon-add-to-cart.svg" alt="add to cart" />
-          Add to Cart
-        </button>
-
-        <button className="hidden flex items-center justify-center absolute bottom-0 left-0 right-0 translate-y-1/2 bg-red-700 w-2/3 m-auto p-2 rounded-full border-0">
-          <button className="w-6 h-6 m-auto border rounded-full flex justify-center items-center text-white cursor-pointer">
-            −
+        {!productExistsInCart ? (
+          <button
+            className="flex items-center justify-center absolute bottom-0 left-0 right-0 translate-y-1/2 bg-white border border-rose-950 rounded-full font-semibold text-rose-950 w-2/3 m-auto p-2 cursor-pointer"
+            onClick={() => addToCart(product)}
+          >
+            <img src="/images/icon-add-to-cart.svg" alt="add to cart" />
+            Add to Cart
           </button>
+        ) : (
+          <div className="flex items-center justify-center absolute bottom-0 left-0 right-0 translate-y-1/2 bg-red-700 w-2/3 m-auto p-2 rounded-full border-0">
+            <button className="w-6 h-6 m-auto border rounded-full flex justify-center items-center text-white cursor-pointer">
+              −
+            </button>
 
-          <input
-            type="number"
-            value={0}
-            readOnly
-            className="w-8 m-auto text-center text-white focus:outline-none appearance-none"
-          />
+            <input
+              type="number"
+              value={product.quantity}
+              readOnly
+              className="w-8 m-auto text-center text-white focus:outline-none appearance-none"
+            />
 
-          <button className="w-6 h-6 m-auto border rounded-full flex justify-center items-center text-white cursor-pointer">
-            +
-          </button>
-        </button>
+            <button 
+              className="w-6 h-6 m-auto border rounded-full flex justify-center items-center text-white cursor-pointer"
+              onClick={() => increaseQuantity(product.id)}
+            >
+              +
+            </button>
+          </div>
+        )}
       </div>
       <div className="my-8">
         <p className="text-rose-300">{product.category}</p>
--- src/hooks/UseCart.js
+++ src/hooks/UseCart.js
@@ -3,6 +3,36 @@ import { productsData } from "../api/api";
 
 export const UseCart = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    const productExits = cart.findIndex(element => element.id === product.id);
+    if(productExits >= 0) {
+      const updatedCart = cart.map(element => {
+        if(element.id === product.id) {
+          return {...element, quantity: element.quantity + 1}
+        }
+        return element;
+      })
+
+      setCart(updatedCart)
+    }else{
+      product.quantity = 1;
+      setCart([...cart, product])
+    }
+  }
+
+  const increaseQuantity = (id) => {
+    const updatedCart = [...cart]
+    updatedCart.map(element => {
+      if(element.id === id) {
+        return element.quantity ++;
+      }
+      return element;
+    })
+
+    setCart(updatedCart);
+  }
 
   useEffect(() => {
     const getProducts = async () => {
@@ -19,5 +49,8 @@ export const UseCart = () => {
 
   return {
     products,
+    cart,
+    addToCart,
+    increaseQuantity
   };
 };
